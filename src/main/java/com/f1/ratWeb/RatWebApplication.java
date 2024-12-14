22344764831075b280515545d34d1c1ba4558461
msl.trp@gmail.com <msl.trp@gmail.com>
2024-12-14 06:54:54 -0300

    Atualizando Projeto

---
+++ src/main/java/com/f1/ratWeb/RatWebApplication.java
@@ -0,0 +1,13 @@
+package com.f1.ratWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatWebApplication.class, args);
+	}
+
+}
